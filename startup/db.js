892ecefe33e8626ec64e42d82ccc932d78e7cd6f
Angel Ismael Linares <angel1sma3l@hotmail.com>
2019-11-23 15:03:45 -0500

    db change 5

--- startup/db.js
+++ startup/db.js
@@ -4,16 +4,17 @@ const winston = require('winston');
 
 module.exports = function() {
 
-    const db = config.get('db');
-    // connect to db
-mongoose.connect(db)
-.then(() => winston.info( `Connected to ${db}...`));
-
-}
-//REQUIRED FOR mongodb@3.x.x
+    //REQUIRED FOR mongodb@3.x.x
 // Optional. Use this if you create a lot of connections and don't want
 // to copy/paste `{ useNewUrlParser: true }`.
 mongoose.set('useNewUrlParser', true); 
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
+
+    const db = config.get('db');
+    // connect to db
+mongoose.connect(db)
+.then(() => winston.info( `Connected to ${db}...`));
+
+}
